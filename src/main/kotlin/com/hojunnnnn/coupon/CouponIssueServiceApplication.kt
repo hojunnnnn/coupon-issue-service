9268d42e409044e562dc58c6d49b9f00b8fa9ae9
HJLee <gotjd8515@gmail.com>
2025-10-11 15:17:43 +0900

    Initial commit

---
+++ src/main/kotlin/com/hojunnnnn/coupon/CouponIssueServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.hojunnnnn.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponIssueServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponIssueServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/hojunnnnn/coupon/CouponIssueServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hojunnnnn.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponIssueServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
