2f627c194b8c98b2990fb0670d52bfb3e06de3b6
HJLee <gotjd8515@gmail.com>
2025-10-24 11:58:48 +0900

    reentrantLock 적용

--- src/main/kotlin/com/hojunnnnn/coupon/application/service/CouponLockManager.kt
+++ src/main/kotlin/com/hojunnnnn/coupon/application/service/CouponLockManager.kt
@@ -2,16 +2,25 @@ package com.hojunnnnn.coupon.application.service
 
 import com.hojunnnnn.coupon.domain.UserCoupon
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantLock
 
 @Component
 class CouponLockManager(
     private val couponIssuer: CouponIssuer,
+    private val reentrantLock: ReentrantLock = ReentrantLock(),
 ) {
 
     fun issueCoupon(userId: String, couponId: Long): UserCoupon {
-        synchronized(this) {
-            // Transaction commit 이후 락 해제
+
+        if (!reentrantLock.tryLock(2, TimeUnit.SECONDS)) {
+            throw Exception("쿠폰 발급이 지연되고 있습니다. 잠시 후 다시 시도해주세요.")
+        }
+
+        try {
             return couponIssuer.issueCoupon(userId, couponId)
+        } finally {
+            reentrantLock.unlock()
         }
     }
 
